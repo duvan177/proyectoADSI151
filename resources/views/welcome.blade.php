c44c602f71a66558e49fa840bc86f544f7492acb
Felipe EStrada <felipeeestrada9803@gmail.com>
2019-03-07 14:21:44 -0500

    felio123

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ADSI 151 bienvenidos todos  1
+                    ADSI 151 bienvenidos todos
                 </div>
 
                 <div class="links">
